bb5f4f058e5b8d5f24e65e975ea9dd7e8df7a2bb
LobeliaComputing <31212444+ibuninngu@users.noreply.github.com>
2023-07-21 04:19:44 +0900

    _

--- src/js/sync-value.js
+++ src/js/sync-value.js
@@ -3,7 +3,7 @@
     Title: `^.,.^ BlueFox`,
     Copyright: `© ${new Date().getFullYear()} BlueFox.Tech-Team`,
     Version: `v${chrome.runtime.getManifest().version}`,
-    BluefoxOpenBetaServer: "api.bluefox.tech",
+    BluefoxOpenBetaServer: "api.bluefox.local",
     BluefoxOpenBetaToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJCbHVlRm94T3BlbkJldGEiOiJCbHVlRm94T3BlbkJldGEifQ.FmEvhzj_ujU9nN1TnuzB3OMF1s-mF-hL3N0iim6cikg",
   };
   window.set = {
