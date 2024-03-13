b74b8dd4102e3c6b63e5aed73c508444697854fd
LobeliaComputing <31212444+ibuninngu@users.noreply.github.com>
2024-03-13 20:06:32 +0900

    _

--- content/js/document_start.js
+++ content/js/document_start.js
@@ -12,12 +12,404 @@
       window.addEventListener("load", load);
     });
 
+    let BlueFoxJs = await new Promise((resolve) => {
+      let BlueFoxJsReady = async (event) => {
+        window.removeEventListener("BlueFoxJs@Ready", BlueFoxJsReady);
+        resolve(event.detail.BlueFoxJs);
+      };
+      window.addEventListener("BlueFoxJs@Ready", BlueFoxJsReady);
+    });
+    let BlueFox = new BlueFoxJs.Automation.BlueFox();
+
+    let log = console.log;
+
+    BlueFox.captureDOM = async (
+      file_name,
+      element,
+      window_object,
+      format = "jpeg",
+      quality = 50
+    ) => {
+      let domRect = element.getBoundingClientRect();
+      let R = await chrome.runtime.sendMessage({
+        type: "Page.captureScreenshot",
+        object: {
+          format: format,
+          quality: quality,
+          clip: {
+            x: domRect.x,
+            y: domRect.top + window_object.scrollY,
+            width: domRect.width,
+            height: domRect.height,
+            scale: 1,
+          },
+          captureBeyondViewport: true,
+        },
+      });
+      Object.assign(document.createElement("a"), {
+        href: `data:image/png;base64,${R}`,
+        download: `${file_name}.${format}`,
+      }).click();
+    };
+    BlueFox.dispatchKeyEvent = async (o) => {
+      let R = await chrome.runtime.sendMessage({
+        type: "Input.dispatchKeyEvent",
+        object: o,
+      });
+      return R;
+    };
+
+    let EventFilter = {
+      Event: (event) => {
+        return {
+          type: event.type,
+          eventPrototype: Object.prototype.toString.call(event).slice(8, -1),
+          timestamp: event.timeStamp,
+          property: {
+            bubbles: event.bubbles,
+            cancelable: event.cancelable,
+            composed: event.composed,
+          },
+        };
+      },
+      InputEvent: (event) => {
+        return {
+          type: event.type,
+          eventPrototype: Object.prototype.toString.call(event).slice(8, -1),
+          timestamp: event.timeStamp,
+          property: {
+            bubbles: event.bubbles,
+            cancelable: event.cancelable,
+            composed: event.composed,
+
+            // UIEvent
+            detail: event.detail,
+            // view: event.view,
+
+            inputType: event.inputType,
+            data: event.data,
+            isComposing: event.isComposing,
+            detail: event.detail,
+          },
+        };
+      },
+      KeyboardEvent: (event) => {
+        return {
+          type: event.type,
+          eventPrototype: Object.prototype.toString.call(event).slice(8, -1),
+          timestamp: event.timeStamp,
+          property: {
+            bubbles: event.bubbles,
+            cancelable: event.cancelable,
+            composed: event.composed,
+            // UIEvent
+            detail: event.detail,
+            key: event.key,
+            code: event.code,
+            location: event.location,
+            repeat: event.repeat,
+            isComposing: event.isComposing,
+            charCode: event.charCode,
+            keyCode: event.keyCode,
+            which: event.which,
+            ctrlKey: event.ctrlKey,
+            shiftKey: event.shiftKey,
+            altKey: event.altKey,
+            metaKey: event.metaKey,
+          },
+        };
+      },
+      FocusEvent: (event) => {
+        return {
+          type: event.type,
+          eventPrototype: Object.prototype.toString.call(event).slice(8, -1),
+          timestamp: event.timeStamp,
+          property: {
+            bubbles: event.bubbles,
+            cancelable: event.cancelable,
+            composed: event.composed,
+            // UIEvent
+            detail: event.detail,
+          },
+        };
+      },
+      MouseEvent: (event) => {
+        return {
+          type: event.type,
+          eventPrototype: Object.prototype.toString.call(event).slice(8, -1),
+          timestamp: event.timeStamp,
+          property: {
+            bubbles: event.bubbles,
+            cancelable: event.cancelable,
+            composed: event.composed,
+            // UIEvent
+            detail: event.detail,
+            screenX: event.screenX,
+            screenY: event.screenY,
+            clientX: event.clientX,
+            clientY: event.clientY,
+            ctrlKey: event.ctrlKey,
+            shiftKey: event.shiftKey,
+            altKey: event.altKey,
+            metaKey: event.metaKey,
+            button: event.button,
+            buttons: event.buttons,
+            region: event.region,
+          },
+        };
+      },
+    };
+
+    let CaptureType = {
+      click: EventFilter.MouseEvent,
+      dblclick: EventFilter.MouseEvent,
+      drop: EventFilter.MouseEvent,
+      dragover: EventFilter.MouseEvent,
+      dragenter: EventFilter.MouseEvent,
+      pointerdown: EventFilter.MouseEvent,
+      beforeinput: EventFilter.InputEvent,
+      focus: EventFilter.FocusEvent,
+      focusin: EventFilter.FocusEvent,
+      focusout: EventFilter.FocusEvent,
+      keydown: EventFilter.KeyboardEvent,
+      keypress: EventFilter.KeyboardEvent,
+      keyup: EventFilter.KeyboardEvent,
+      input: EventFilter.InputEvent,
+      change: EventFilter.Event,
+    };
+
     let messageHandler = {
+      "BlueFox.Dispatch": async (message, connector) => {
+        let R;
+        for (let f of message.object.files) {
+          await {
+            "application/json": async (_) => {
+              R = await BlueFox.do(JSON.parse(await _.text));
+            },
+            "text/javascript": async (_) => {
+              await chrome.runtime.sendMessage({
+                type: "Runtime.evaluate",
+                object: {
+                  expression: await _.text,
+                  objectGroup: "BlueFox-js-lanch",
+                  awaitPromise: true,
+                  returnByValue: true,
+                },
+              });
+            },
+          }[f.type](f);
+        }
+        return R;
+      },
+      "BlueFox.Dispatch.Action": async (message, connector) => {
+        return await BlueFox.do(JSON.parse(message.object));
+      },
+      "BlueFox.Dispatch.Script": async (message, connector) => {
+        let R = await chrome.runtime.sendMessage({
+          type: "Runtime.evaluate",
+          object: {
+            expression: message.object,
+            objectGroup: "BlueFox-js-lanch",
+            awaitPromise: true,
+            returnByValue: true,
+          },
+        });
+        return R;
+      },
+      "BlueFox.CaptureWindow": async (message, connector) => {
+        let R = await chrome.runtime.sendMessage({
+          type: "Page.captureScreenshot",
+          object: message.object,
+        });
+        return R;
+      },
+      "BlueFox.GetEventListners": async (message, connector) => {
+        let R = (
+          await chrome.runtime.sendMessage({
+            type: "DOMDebugger.getEventListeners",
+            object: {
+              objectId: (
+                await chrome.runtime.sendMessage({
+                  type: "Runtime.evaluate",
+                  object: {
+                    expression: "(()=>{return document;})()",
+                    objectGroup: "event-listeners-test",
+                  },
+                })
+              ).result.objectId,
+              depth: -1,
+            },
+          })
+        ).listeners;
+        return R;
+      },
+      "BlueFox.GetSelectors": async (message, connector) => {
+        let R = [];
+        try {
+          [...document.querySelectorAll(message.object.selector)].forEach((_) => {
+            try {
+              R.push({
+                selector: CssSelectorGenerator.getCssSelector(_),
+                attributes: (() => {
+                  let r = {};
+                  [..._.attributes]
+                    .map((attribute) => {
+                      return {
+                        name: attribute.name,
+                        value: attribute.value,
+                      };
+                    })
+                    .forEach((attribute) => {
+                      r[attribute.name] = attribute.value;
+                    });
+                  return r;
+                })(),
+              });
+            } catch { }
+          });
+        } catch { }
+        return R;
+      },
+      "BlueFox.GetElementProperties": async (message, connector) => {
+        let R = {};
+        let select = (selector) => {
+          if (selector[0] == "/") {
+            return document.evaluate(
+              selector,
+              document,
+              null,
+              XPathResult.FIRST_ORDERED_NODE_TYPE,
+              null,
+            ).singleNodeValue;
+          }
+          else {
+            return document.querySelector(selector);
+          }
+        };
+        try {
+
+          let target = select(message.object.selector);
+          for (let property in target) {
+            R[property] = target[property];
+          }
+          R.attributes = {};
+          [...target.attributes].forEach((attribute) => {
+            R.attributes[attribute.name] = {
+              name: attribute.name,
+              value: attribute.value
+            };
+          });
+
+        } catch { }
+        return R;
+      },
+      "BlueFox.CaptureDOMSnapshot": async (message, connector) => {
+        let R = await chrome.runtime.sendMessage({
+          type: "DOMSnapshot.captureSnapshot",
+          object: message.object,
+        });
+        return R;
+      },
+      "BlueFoxScript.AddEventListener": async (message, connector) => {
+        try {
+          let R = [];
+          let elements = [];
+          if (message.object.selector[0] == "/") {
+            let result = document.evaluate(
+              message.object.selector,
+              document,
+              null,
+              XPathResult.UNORDERED_NODE_ITERATOR_TYPE,
+              null,
+            );
+            elements = [...(
+              function* () {
+                while ((node = result.iterateNext())) {
+                  yield node;
+                }
+              }
+            )(result)];
+          } else {
+            elements = [...document.querySelectorAll(message.object.selector)];
+          }
+          elements.forEach((_) => {
+            let selector = CssSelectorGenerator.getCssSelector(_);
+            let uuid = crypto.randomUUID();
+            _.addEventListener(
+              message.object.event_type,
+              (event) => {
+                connector.postMessage(
+                  Object.assign(
+                    message,
+                    {
+                      object: {
+                        uuid: message.object.uuid,
+                        object: {
+                          uuid: uuid,
+                          type: message.object.type,
+                          event: (() => {
+                            try {
+                              return Object.assign(
+                                CaptureType[event.type](event),
+                                {
+                                  target: selector,
+                                }
+                              );
+                            } catch (e) {
+                              return {
+                                type: message.object.event_type,
+                                target: selector,
+                              };
+                            }
+                          })(),
+                          properties: (() => {
+                            let r = {};
+                            for (let property in _) {
+                              r[property] = _[property];
+                            }
+                            r.attributes = {};
+                            [..._.attributes].forEach((attribute) => {
+                              r.attributes[attribute.name] = {
+                                name: attribute.name,
+                                value: attribute.value
+                              };
+                            });
+                            return r;
+                          })()
+                        }
+                      },
+                    }
+                  )
+                );
+              }
+            );
+
+            R.push(
+              {
+                uuid: uuid,
+                selector: selector,
+              }
+            );
+          })
+          return R;
+        } catch (e) {
+          return [];
+        }
+      },
       "Tab.windowOnLoad": async (message, connector) => {
         sessionStorage.uuid = await windowOnLoad;
         return sessionStorage.uuid;
-      }
+      },
     };
+    await chrome.runtime.sendMessage({
+      type: "Debugger.attach",
+    });
+    setInterval(async () => {
+      await chrome.runtime.sendMessage({
+        type: "Debugger.attach",
+      });
+    }, 5000);
+
     chrome.runtime.onConnect.addListener((connector) => {
       connector.onMessage.addListener((message) => {
         (async () => {
